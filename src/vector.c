9c25df14a346f2bf55b45be2324a28a1669fabaf
Cannon Lewis <cannon10100@gmail.com>
2015-01-28 17:53:26 -0600

    Finished implementing vector, time to test

--- src/vector.c
+++ src/vector.c
@@ -6,6 +6,8 @@
  */
 
 #include "vector.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 Motor_vector* init_Motor_vector() {
 	Motor_vector* vect = malloc(sizeof(Motor_vector));
@@ -16,7 +18,7 @@ Motor_vector* init_Motor_vector() {
 
 int Motor_vector_append(Motor_vector* vect, Motor* element) {
 	vect->data[vect->elem_current] = element;
-	++vect->elem_current;
+	vect->elem_current++;
 	if(vect->elem_current >= vect->elem_total) {
 		Motor* new_data = realloc(vect->data, (vect->elem_total * 2) * sizeof(Motor));
 		if(new_data) {
@@ -31,8 +33,15 @@ int Motor_vector_append(Motor_vector* vect, Motor* element) {
 	return 1;
 }
 
-/*
-Motor* Motor_vector_get(Motor_vector* vect, int index) {
 
+Motor* Motor_vector_get(Motor_vector* vect, int index) {
+	Motor* return_elem;
+	if(index < vect->elem_current & index >= 0) {
+		return_elem = vect->data[index];
+	} else {
+		fprintf(stderr, "Index not in vector");
+		exit(EXIT_FAILURE);
+	}
+	return return_elem;
 }
-*/
+
